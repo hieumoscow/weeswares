3552d0de3a9b4ce0e76f4a9bad033edde1142fe8
Nguyen Nhu Hieu <hieumoscow89@yahoo.com>
2018-06-14 10:54:35 +0800

    Initial commit

---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild Microsoft.Bot.Sample.LuisBot.sln -p:DeployOnBuild=true -p:PublishProfile=hieuwwbot-Web-Deploy.pubxml -p:Password=Dr8Kj5Z8d4T7DtyRHl75Tk4T4PPgFpe8bZSZJAbz5eibLsXzMn7JmwFPkAxs
+
